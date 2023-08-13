25f8cc449f5160a335e1b7fdf21d420cbe4bd5c1
Quazi Mushroof Abdullah <69722621+qmamushroof@users.noreply.github.com>
2023-08-13 17:09:00 +0600

    Add files via upload

---
+++ adminmembermanagement.aspx.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Online_Library_Management
+{
+    public partial class adminmembermanagement : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        DateTime dueDate;
+        String string_due_date;
+        int daysDifference,calculated_fine,totalFine;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridView1.DataBind();
+        }
+        // Go button
+        protected void LinkButton4_Click(object sender, EventArgs e)
+        {
+            getMemberByID();
+        }
+        // Active button
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            updateMemberStatusByID("active");
+        }
+        // pending button
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+            updateMemberStatusByID("pending");
+        }
+        // deactive button
+        protected void LinkButton3_Click(object sender, EventArgs e)
+        {
+            updateMemberStatusByID("deactive");
+        }
+        // delete button
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            deleteMemberByID();
+        }
+
+
+
+
+
+        // user defined function
+
+        bool checkIfMemberExists()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("SELECT * from member_master_tbl where member_id='" + TextBox1.Text.Trim() + "';", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+                return false;
+            }
+        }
+
+        void deleteMemberByID()
+        {
+            if (checkIfMemberExists())
+            {
+                try
+                {
+                    SqlConnection con = new SqlConnection(strcon);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+
+                    SqlCommand cmd = new SqlCommand("DELETE from member_master_tbl WHERE member_id='" + TextBox1.Text.Trim() + "'", con);
+
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    Response.Write("<script>alert('Member Deleted Successfully');</script>");
+                    clearForm();
+                    GridView1.DataBind();
+
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert('" + ex.Message + "');</script>");
+                }
+
+            }
+            else
+            {
+                Response.Write("<script>alert('Invalid Member ID');</script>");
+            }
+        }
+
+        void getMemberByID()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+
+                }
+
+                //fine calculation and insertion
+                SqlCommand cmd1 = new SqlCommand("SELECT due_date from book_issue_tbl where member_id='" + TextBox1.Text.Trim() + "'", con);
+                SqlDataReader reader = cmd1.ExecuteReader();
+                while (reader.Read())
+                {
+                    DateTime dueDate = Convert.ToDateTime(reader["due_date"]);
+
+                    int daysDifference = (int)(DateTime.Today - dueDate).TotalDays;
+                    int calculatedFine = Math.Max(0, 10 * daysDifference); // Ensure the fine is non-negative
+
+                    totalFine += calculatedFine;
+                }
+                reader.Close();
+                SqlCommand cmd2 = new SqlCommand("UPDATE member_master_tbl SET fine='" + totalFine + "' WHERE member_id='" + TextBox1.Text.Trim() + "'", con);
+                cmd2.ExecuteNonQuery();
+
+                //if (DateTime.TryParse(string_due_date, out dueDate))
+                //{
+                //    // Calculate the difference between the parsed date and today's date
+                //    TimeSpan difference = DateTime.Today - dueDate;
+                //    daysDifference = difference.Days;
+                //}
+                //if (daysDifference >= 0)
+                //{
+                //    calculated_fine = 10 * daysDifference;
+                //}
+                //else
+                //{
+                //    calculated_fine = 0;
+                //}
+
+                SqlCommand cmd = new SqlCommand("select * from member_master_tbl where member_id='" + TextBox1.Text.Trim() + "'", con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        TextBox2.Text = dr.GetValue(0).ToString();
+                        TextBox7.Text = dr.GetValue(10).ToString();
+                        TextBox8.Text = dr.GetValue(1).ToString();
+                        TextBox3.Text = dr.GetValue(2).ToString();
+                        TextBox4.Text = dr.GetValue(3).ToString();
+                        TextBox9.Text = dr.GetValue(4).ToString();
+                        TextBox10.Text = dr.GetValue(5).ToString();
+                        TextBox11.Text = dr.GetValue(6).ToString();
+                        TextBox6.Text = dr.GetValue(7).ToString();
+                        //TextBoxFine.Text=dr.GetValue(9).ToString();
+
+                    }
+
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid credentials');</script>");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        void updateMemberStatusByID(string status)
+        {
+            if (checkIfMemberExists())
+            {
+                try
+                {
+                    SqlConnection con = new SqlConnection(strcon);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+
+                    }
+                    SqlCommand cmd = new SqlCommand("UPDATE member_master_tbl SET account_status='" + status + "' WHERE member_id='" + TextBox1.Text.Trim() + "'", con);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    GridView1.DataBind();
+                    Response.Write("<script>alert('Member Status Updated');</script>");
+
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert('" + ex.Message + "');</script>");
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('Invalid Member ID');</script>");
+            }
+
+        }
+
+        void clearForm()
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox7.Text = "";
+            TextBox8.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            TextBox9.Text = "";
+            TextBox10.Text = "";
+            TextBox11.Text = "";
+            TextBox6.Text = "";
+        }
+
+    }
+}
---
+++ homepage.aspx
@@ -0,0 +1,82 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="homepage.aspx.cs" Inherits="Online_Library_Management.homepage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <section>
+      <img width="100%" src="imgs/home-bg.jpg" class="img-fluid"/>
+   </section>
+   <section>
+      <div class="container">
+         <div class="row">
+            <div class="col-12">
+               <center>
+                  <h2>Features</h2>
+                  <p><b></b></p>
+               </center>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/digital-inventory.png"/>
+                  <h4>Digital Book Inventory</h4>
+                  <p class="text-justify">Browse our collection of books</p>
+               </center>
+            </div>
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/search-online.png"/>
+                  <h4>Search Books</h4>
+                  <p class="text-justify">Search books in our library</p>
+               </center>
+            </div>
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/defaulters-list.png"/>
+                  <h4>Defaulter List</h4>
+                  <p class="text-justify">List of memebers who have been fined</p>
+               </center>
+            </div>
+         </div>
+      </div>
+   </section>
+   <section>
+      <img src="imgs/in-homepage-banner.jpg" class="img-fluid"/>
+   </section>
+   <section>
+      <div class="container">
+         <div class="row">
+            <div class="col-12">
+               <center>
+                  <h2>Process</h2>
+                  <p><b></b></p>
+               </center>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/sign-up.png" />
+                  <h4>Sign Up</h4>
+                  <p class="text-justify">Sign up and borrow books</p>
+               </center>
+            </div>
+            <div class="col-md-4">
+                <center>
+                  <img width="150px" src="imgs/generaluser.png"/>
+                  <h4>Log in</h4>
+                  <p class="text-justify">You can log in if the admin approves</p>
+               </center>
+               
+            </div>
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/library.png"/>
+                  <h4>Borrow Books</h4>
+                  <p class="text-justify">Borrow books from our collection</p>
+               </center>
+            </div>
+         </div>
+      </div>
+   </section>
+</asp:Content>
